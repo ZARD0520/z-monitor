334ea60924dff69d5e53c2b929f3b0ffb2e8c2f5
ZARD0520 <422451702@qq.com>
2024-10-21 23:31:05 +0800

    init platform for vue

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,1 +1,34 @@
-console.log('core ok')
+export const defaultPluginConfig = {
+  ajax: {
+    open: true, // 是否开启此插件
+    req: true, // 是否开启对请求参数的记录
+    customMethod: (data, [ajax]) => { return data } // 自定义处理ajax数据方法
+  },
+  log: {
+    open: true,
+    type: 'time', // type为time时，用时间来控制上传频率；type为num时，则用采集次数控制
+    time: 30 * 1000, // 隔30s上传一次日志
+    MAX_HTTP_FAIL: 3, // 超过3次失败关闭监控，服务端接口可能错误
+    customMethod: (item) => { return item }
+  },
+  http: {
+    open: true,
+    isCustomRequest: false,
+    requestConfig: {
+      url: '', // 请求地址
+      method: '', // 请求类型：POST、GET等
+      headers: {}, // 请求头配置
+    },
+    customMethod: (data, done) => { }, // 自定义请求，data为采集上报的参数数据
+  },
+  userInfo: {
+    open: false
+  },
+  videoRecord: {
+    open: false,
+    waitTime: 2000, // 延迟多久上报，采集报错后的内容
+    checkoutEveryNth: 300, // 每N个数据做切片
+  }
+}
+
+export class Monitor { }
--- packages/vue/src/index.js
+++ packages/vue/src/index.js
@@ -1,1 +1,26 @@
-console.log('vue ok')
+import { defaultPluginConfig, Monitor } from '@libc/core'
+
+const defaultConfig = {
+  platform: 'vue2', // 监听的平台，默认为Vue2
+  key: 'z-monitor-' + Date.now().toString() // 唯一key
+}
+
+export default function createMonitor(config = defaultConfig, pluginConfig = defaultPluginConfig) {
+  try {
+    const mergeConfig = {
+      key: config.key,
+      plugins: {}
+    }
+    Object.keys(pluginConfig).forEach((plugin) => {
+      if (plugin.open) {
+        mergeConfig.plugins[plugin] = pluginConfig[plugin]
+      }
+    })
+    const monitor = new Monitor(mergeConfig)
+    // 注册平台监控
+    register(config.platform)(monitor)
+    // 分别注册插件
+  } catch (e) {
+    console.error(e, 'monitor错误')
+  }
+}
