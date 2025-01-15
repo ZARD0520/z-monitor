8a3cbd7d189465bf88b714f4a22d2885962179c1
huangjunjie <huangjunjie@newlang.cn>
2025-01-15 21:10:41 +0800

    update：remove dts

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -1,8 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import dts from 'rollup-plugin-dts';
 
-export default [{
+export default {
   input: 'src/index.js',
   output: {
     name: 'vue',
@@ -16,11 +15,4 @@ export default [{
     }),
     terser(), // 代码混淆插件
   ],
-},{
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.d.ts',
-    format: 'esm',
-  },
-  plugins: [dts()]
-}];
+}
