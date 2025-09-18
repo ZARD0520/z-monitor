cbdd41233d27dad90ca35c2b02c4a0d13c580840
huangjunjie <huangjunjie@newlang.cn>
2025-09-18 16:55:39 +0800

    update: 调整打包配置

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -1,23 +1,49 @@
+import { defineConfig } from 'rollup';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    name: 'vue',
-    file: 'dist/index.js',
-    format: 'esm',
+export default defineConfig([
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.esm.min.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**', // 排除 node_modules
+        babelHelpers: 'bundled'
+      }),
+      resolve(),
+      commonjs(),
+      terser({ 
+        module: true,
+        compress: { passes: 2 },
+        mangle: true
+      })
+    ]
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**', // 排除 node_modules
-      babelHelpers: 'bundled'
-    }),
-    resolve(),
-    commonjs(),
-    terser(), // 代码混淆插件
-    // TODO: 生产包配置去除console插件
-  ]
-}
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.cjs.min.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**', // 排除 node_modules
+        babelHelpers: 'bundled'
+      }),
+      resolve(),
+      commonjs(),
+      terser({
+        compress: { passes: 2 },
+        mangle: true
+      }),
+    ]
+  }
+])
